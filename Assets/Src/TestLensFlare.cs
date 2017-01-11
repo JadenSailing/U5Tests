64cfb14f195d569dfcc802d6850f86cac009ddc9
Jaden <JadenSailing@126.com>
2017-01-11 15:55:50 +0800

    LensFlare

---
+++ Assets/Src/TestLensFlare.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// 镜头光晕
+/// </summary>
+public class TestLensFlare : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        GameObject obj = Resources.Load("Other/LensFlare/LensFlare") as GameObject;
+        GameObject inst = GameObject.Instantiate(obj);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+}
