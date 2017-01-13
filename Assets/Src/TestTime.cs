ff311738bed9a6e9e87fbf10eda7fcc77a6de4f3
Jaden <JadenSailing@126.com>
2017-01-13 15:53:12 +0800

    TestTime

---
+++ Assets/Src/TestTime.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Time.timeScale影响Time.time数值 不影响Time.realtimeSinceStartup
+/// </summary>
+public class TestTime : MonoBehaviour {
+
+    private float _currentTime = 0.0f;
+    private float _currentRealTime = 0.0f;
+
+    // Use this for initialization
+    void Start () {
+        GameObject psObj = new GameObject("Ps");
+        psObj.AddComponent<ParticleSystem>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        _currentTime = Time.time;
+        _currentRealTime = Time.realtimeSinceStartup;
+
+    }
+
+    private void OnGUI()
+    {
+        if(GUI.Button(new Rect(100, 0, 200, 100), "Rate+0.1"))
+        {
+            float t = Time.timeScale * 1.1f;
+            t = t > 100 ? 100 : t;
+            Time.timeScale = t;
+        }
+        if(GUI.Button(new Rect(100, 100, 200, 100), "Rate-0.1"))
+        {
+            float t = Time.timeScale * 1.1f;
+            t = t > 100 ? 100 : t;
+            Time.timeScale = t;
+        }
+
+        GUI.Label(new Rect(500, 200, 100, 100), _currentTime.ToString());
+
+        GUI.Label(new Rect(500, 300, 100, 100), _currentRealTime.ToString());
+    }
+}
