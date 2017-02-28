642dff5e6694d1c0deea640f726d6249921968ca
Jaden <JadenSailing@126.com>
2017-02-28 14:01:06 +0800

    TestType

---
+++ Assets/Src/TestAnimator.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestAnimator : MonoBehaviour {
+
+    public Animator animator;
+	// Use this for initialization
+	void Start () {
+        AnimatorClipInfo clipInfo = animator.GetCurrentAnimatorClipInfo(0)[0];
+        animator.SetTime(0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        animator.SetTime(0);
+    }
+}
---
+++ Assets/Src/Utils/TypeUtil.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+
+/// <summary>
+/// 碰撞检测
+/// </summary>
+public class TypeUtil
+{
+    public static string GetStaticInfo(Type type)
+    {
+        string log = "";
+        //静态属性值
+        PropertyInfo[] infos = type.GetProperties();
+        for (int i = 0; i < infos.Length; i++)
+        {
+            PropertyInfo info = infos[i];
+            log += (type.Name + "." + infos[i].Name + " = " + infos[i].GetValue(null, null)) + "\n";
+        }
+
+        return log;
+    }
+
+    public static string GetFieldInfo(object inst)
+    {
+        Type type = inst.GetType();
+        string log = "";
+        //静态属性值
+        FieldInfo[] infos = type.GetFields();
+        for (int i = 0; i < infos.Length; i++)
+        {
+            FieldInfo info = infos[i];
+            log += type.Name + "." + info.Name + " = " + info.GetValue(inst) + "\n";
+        }
+
+        return log;
+    }
+}
+
+
