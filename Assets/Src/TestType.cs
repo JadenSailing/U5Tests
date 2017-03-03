8502424685c7b469a6cb139e8b37ddde0b5163ed
Jaden <JadenSailing@126.com>
2017-03-03 21:33:20 +0800

    TestType

---
+++ Assets/Src/TestType.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestTypeClass1
+{
+    public int a = 1;
+    public int b
+    {
+        get
+        {
+            return 2;
+        }
+    }
+
+    public static int c
+    {
+        get
+        {
+            return 3;
+        }
+    }
+
+    public static int d = 4;
+    public TestTypeClass1()
+    {
+
+    }
+}
+
+public class TestType : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+        string typeStr = "";
+        typeStr = TypeUtil.GetFieldInfo(new TestTypeClass1());
+        print(typeStr);
+
+        typeStr = TypeUtil.GetPropertyInfo(new TestTypeClass1());
+        print(typeStr);
+
+        typeStr = TypeUtil.GetMemberInfo(new TestTypeClass1());
+        print(typeStr);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    
+}
