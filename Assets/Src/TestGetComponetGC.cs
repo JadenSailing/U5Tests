a77bf605bca99a6156dd07253ee1b38bb5f389ee
Jaden <JadenSailing@126.com>
2017-03-19 15:26:53 +0800

    daily

---
+++ Assets/Src/TestGetComponetGC.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestGetComponetGC : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Transform obj = this.transform;
+        Profiler.BeginSample("GetComponentGC");
+        UIWidget gc = obj.GetComponent<UIWidget>();
+        Profiler.EndSample();
+    }
+}
---
+++ Assets/Src/TestMonoIncrease.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class TestMonoIncrease : MonoBehaviour {
+
+    public GameObject item;
+
+    public List<GameObject> itemList;
+    // Use this for initialization
+    void Start() {
+        itemList = new List<GameObject>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        System.GC.Collect();
+    }
+
+    void OnGUI()
+    {
+        if(GUI.Button(new Rect(0, 0, 200, 100), "Add"))
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+                itemList.Add(GameObject.Instantiate(item));
+            }
+        }
+
+        if (GUI.Button(new Rect(0, 100, 200, 100), "Remove"))
+        {
+            for (int i = 0; i < itemList.Count; i++)
+            {
+                GameObject.Destroy(itemList[i]);
+            }
+            itemList.Clear();
+
+            
+        }
+    }
+}
