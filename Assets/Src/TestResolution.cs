1246b2f07e193bd3ae6ccda603f1e7f39e1668cd
Jaden <JadenSailing@126.com>
2017-03-25 11:29:41 +0800

    tests

--- Assets/Src/TestResolution.cs
+++ Assets/Src/TestResolution.cs
@@ -6,8 +6,16 @@ using System.Collections;
 /// </summary>
 public class TestResolution : MonoBehaviour {
 
+    private float dpi1;
+    private float dpi2;
+
+    private float _width;
+    private float _height;
 	// Use this for initialization
 	void Start () {
+        _width = Screen.width;
+        _height = Screen.height;
+        dpi1 = Screen.dpi;
         Screen.SetResolution(960, 540, true);
 	}
 	
@@ -18,13 +26,27 @@ public class TestResolution : MonoBehaviour {
 
     void OnGUI()
     {
+        dpi2 = Screen.dpi;
         int width = Screen.width;
         int height = Screen.height;
         int resWid = Screen.currentResolution.width;
         int resHeight = Screen.currentResolution.height;
+        float mousePosX = Input.mousePosition.x;
+        float mousePosY = Input.mousePosition.y;
+        float mousePosZ = Input.mousePosition.z;
+
+        GUI.Label(new Rect(100, 0, 200, 50), "分辨率宽度1=" + _width);
+        GUI.Label(new Rect(100, 50, 200, 50), "分辨率高度1=" + _height);
+
         GUI.Label(new Rect(100, 100, 200, 50),"分辨率宽度=" + width);
         GUI.Label(new Rect(100, 150, 200, 50), "分辨率高度=" + height);
+
+
         GUI.Label(new Rect(100, 200, 200, 50), "Resolution宽度=" + resWid);
         GUI.Label(new Rect(100, 250, 200, 50), "Resolution高度=" + resHeight);
+        GUI.Label(new Rect(100, 300, 200, 50), "DPI Before=" + dpi1);
+        GUI.Label(new Rect(100, 350, 200, 50), "DPI After=" + dpi2);
+
+        GUI.Label(new Rect(100, 400, 200, 50), "pos" + mousePosX + "-" + mousePosY + "-" + mousePosZ);
     }
 }
