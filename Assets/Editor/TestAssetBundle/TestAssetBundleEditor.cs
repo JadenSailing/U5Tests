f44527aa14473da674cf56148607906ea8365a7a
Jaden <JadenSailing@126.com>
2017-04-03 16:51:14 +0800

    testab

---
+++ Assets/Editor/TestAssetBundle/TestAssetBundleEditor.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+public class TestAssetBundleEditor : Editor {
+
+    private static BuildAssetBundleOptions buildOp = BuildAssetBundleOptions.ChunkBasedCompression | BuildAssetBundleOptions.DeterministicAssetBundle/* | BuildAssetBundleOptions.AppendHashToAssetBundleName*/;// | BuildAssetBundleOptions.CompleteAssets;
+
+    [MenuItem("Tools/BuildScene", false, 12)]
+    public static void BuildScene()
+    {
+        string[] sceneNameList;
+        /*
+        EditorBuildSettingsScene[] t_Scene = EditorBuildSettings.scenes;
+        sceneNameList = new string[t_Scene.Length];
+        for (int i = 0; i < t_Scene.Length; i++)
+        {
+            sceneNameList[i] = t_Scene[i].path;
+        }
+        */
+        sceneNameList = new string[1];
+        sceneNameList[0] = "Assets/Scenes/TestAssetBundleScene1.unity";
+        BuildPipeline.BuildPlayer(sceneNameList, Application.streamingAssetsPath + "/Scenes/" + "Test.Scene", BuildTarget.Android, BuildOptions.BuildAdditionalStreamedScenes);
+
+        AssetDatabase.Refresh();
+        AssetDatabase.SaveAssets();
+    }
+
+    [MenuItem("Tools/BuildFont", false, 12)]
+    public static void BuildFont()
+    {
+        AssetBundleBuild[] abs = new AssetBundleBuild[1];
+        AssetBundleBuild ab = new AssetBundleBuild();
+        ab.assetBundleName = "FZ" + ".ab";
+        string[] AssetNames = new[] { "Assets/Resources/Fonts/FZLTH-GBK.TTF", "Assets/Resources/Fonts/lthj.otf" };
+        ab.assetNames = AssetNames;
+
+        abs[0] = ab;
+        BuildPipeline.BuildAssetBundles(Application.streamingAssetsPath + "/Fonts/", abs, buildOp, BuildTarget.Android);
+    }
+
+    /// <summary>
+    /// 已知依赖
+    /// </summary>
+    [MenuItem("Tools/TestBuild1", false, 12)]
+    public static void TestBuild1()
+    {
+        int max = 5;
+        int index = 0;
+        AssetBundleBuild[] abs = new AssetBundleBuild[max];
+        AssetBundleBuild ab;
+        string[] AssetNames;
+
+        //Texture
+        ab = new AssetBundleBuild();
+        ab.assetBundleName = "icon" + ".ab";
+        AssetNames = new[] { "Assets/Resources/UI/Image/Skill/tianshan02.png" };
+        ab.assetNames = AssetNames;
+        abs[index++] = ab;
+
+        //Build Font
+        ab = new AssetBundleBuild();
+        ab.assetBundleName = "font" + ".ab";
+        AssetNames = new[] { "Assets/Resources/TestAB/Fonts/FZLTH-GBK.TTF"};
+        ab.assetNames = AssetNames;
+        abs[index++] = ab;
+
+        //Build Prefab
+        for (int i = 0; i < 3; i++)
+        {
+            ab = new AssetBundleBuild();
+            ab.assetBundleName = "TestABPanel" + (i+1).ToString() + ".ab";
+            AssetNames = new[] { "Assets/Resources/TestAB/UI/Prefab/TestABPanel" + (i + 1).ToString() + ".prefab" };
+            ab.assetNames = AssetNames;
+            abs[index++] = ab;
+        }
+        BuildPipeline.BuildAssetBundles(Application.streamingAssetsPath + "/TestAB", abs, buildOp, BuildTarget.Android);
+    }
+}
---
+++ Assets/Src/TestAssetBundle.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class TestAssetBundle : MonoBehaviour {
+
+    public GameObject UIRoot;
+
+	// Use this for initialization
+	void Start () {
+
+        /*
+        AssetBundle ab = AssetBundle.LoadFromFile(Application.streamingAssetsPath + "/Scenes/" + "Test.Scene");
+        SceneManager.LoadScene("TestAssetBundleScene1");
+        */
+
+        //加载UI
+        AssetBundle fontAB = AssetBundle.LoadFromFile(Application.streamingAssetsPath + "/TestAB/" + "font.ab");
+        AssetBundle iconAB = AssetBundle.LoadFromFile(Application.streamingAssetsPath + "/TestAB/" + "icon.ab");
+
+        AssetBundle uiAB = AssetBundle.LoadFromFile(Application.streamingAssetsPath + "/TestAB/" + "testabpanel3.ab");
+        string assetName = "TestAbpanel3";
+        GameObject prefab = uiAB.LoadAsset(assetName) as GameObject;
+
+        GameObject obj = GameObject.Instantiate(prefab);
+        this.Add2UIRoot(obj);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void Add2UIRoot(GameObject obj)
+    {
+        obj.transform.parent = UIRoot.transform;
+        obj.transform.localScale = Vector3.one;
+        obj.transform.localPosition = Vector3.zero;
+        obj.transform.localRotation = Quaternion.identity;
+    }
+}
