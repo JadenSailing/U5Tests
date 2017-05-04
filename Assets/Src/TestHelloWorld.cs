c254a18f7456b222dd5173d84f02fe83bea9e66d
Jaden <JadenSailing@126.com>
2017-05-04 21:04:42 +0800

    all files name

--- Assets/Src/TestHelloWorld.cs
+++ Assets/Src/TestHelloWorld.cs
@@ -1,11 +1,32 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.IO;
+using System.Collections.Generic;
+using System;
 
 public class TestHelloWorld : MonoBehaviour {
 
+    private string SrcPath = "/Src";
 	// Use this for initialization
 	void Start () {
         Debug.Log("Hello World");
+
+        SrcPath = Application.dataPath + SrcPath;
+        string allFiles = "";
+        if(Directory.Exists(SrcPath))
+        {
+            string[] files = Directory.GetFiles(SrcPath, "*.cs", SearchOption.TopDirectoryOnly);
+            Array.Sort(files);
+            for (int i = 0; i < files.Length; i++)
+            {
+                string fileName = files[i];
+                int lastIndex = fileName.LastIndexOf("\\");
+                fileName = fileName.Substring(lastIndex + 1, fileName.Length - lastIndex - 4);
+                string index = (i+1) < 10 ? "0" + (i + 1).ToString() : (i + 1).ToString();
+                allFiles = allFiles + index + "." + fileName + "\n\r";
+            }
+        }
+        Debug.Log(allFiles);
 	}
 	
 	// Update is called once per frame
