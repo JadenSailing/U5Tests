14d341705a5fafa0d22c3d61a13fbf4d6398d857
Jaden <JadenSailing@126.com>
2017-05-12 14:28:30 +0800

    texturepot

---
+++ Assets/Src/TestTextureRect.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Texture WrapMode Repeat模式下 必须使用PoT模式否则会失效
+/// </summary>
+public class TestTextureRect : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
